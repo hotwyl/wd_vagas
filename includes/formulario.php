7acb70a6a78615fe114d71eb147d0d834e8a1482
William Camargo <william_galdino@econeteditora.com.br>
2021-08-18 13:19:40 -0300

    inicio

---
+++ includes/formulario.php
@@ -0,0 +1,16 @@
+<main>
+
+<section>
+    <a href="index.php">
+    <button class="btn btn-success">Voltar</button>
+    </a>
+</section>
+
+<h2 class="mt-3">Cadastrar Vaga</h2>
+
+<form method="post">
+    <div class="form-group">
+    </div>
+</form>
+
+</main>
