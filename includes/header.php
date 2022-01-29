f3a66118d5554a5ae810b870ae71295adf979117
William Camargo <44587916+hotwyl@users.noreply.github.com>
2022-01-29 20:40:10 -0300

    renomear

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@ $usuario = $usuarioLogado ?
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-  <title>WDEV Vagas!</title>
+  <title>CRUD - Vagas</title>
 </head>
 
 <body class="bg-dark text-light">
@@ -31,7 +31,7 @@ $usuario = $usuarioLogado ?
   <div class="container">
 
     <div class="jumbotron bg-danger">
-      <h1>WDEV Vagas</h1>
+      <h1>CRUD - Vagas</h1>
       <p> Exemplo de CRUD com PHP orientado a objetos</p>
 
       <hr class="border-light">
